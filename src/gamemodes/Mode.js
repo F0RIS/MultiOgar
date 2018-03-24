940be3f0c3fc99eeb247df478082b14b861db34f
Tom <36286279+agarian-2@users.noreply.github.com>
2018-03-24 19:52:40 -0400

    Add the 'gamemode' commands's 'onChange' function
    Links to FFA, Teams, and Rainbow FFA.

--- src/gamemodes/Mode.js
+++ src/gamemodes/Mode.js
@@ -32,3 +32,10 @@ Mode.prototype.onCellMove = function(cell, gameServer) {};
 Mode.prototype.updateLB = function(gameServer) {
     gameServer.leaderboardType = this.packetLB;
 };
+
+Mode.prototype.onChange = function(gameServer) {
+    for (;gameServer.nodes.all.length;) gameServer.removeNode(gameServer.nodes.all[0]);
+    for (;gameServer.nodes.eject.length;) gameServer.removeNode(gameServer.nodes.eject[0]);
+    for (;gameServer.nodes.food.length;) gameServer.removeNode(gameServer.nodes.food[0]);
+    for (;gameServer.nodes.virus.length;) gameServer.removeNode(gameServer.nodes.virus[0]);
+};
